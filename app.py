96f3d55c539e415a2e6888e0792ad8ec89d3f589
Preston Davis <pdavis@pebcac.org>
2024-01-17 15:55:47 -0500

    Updated base python app

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
-import os
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+AI_CONTAINER_1 = 'http://gowebapp1.test.svc.cluster.local'
+AI_CONTAINER_2 = 'http://gowebapp2.test.svc.cluster.local'
+
+@app.route('/<container_id>')
+def proxy(container_id):
+    if container_id == '1':
+        response = requests.get(AI_CONTAINER_1)
+    elif container_id == '2':
+        response = requests.get(AI_CONTAINER_2)
+    else:
+        return jsonify({"error": "Invalid container ID"}), 404
+
+    llm_info = response.headers.get('LLM-Header', 'Not provided')
+    return jsonify({
+        "service_called": container_id,
+        "llm_info": llm_info,
+        "original_response": response.json()
+    })
 
 if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
